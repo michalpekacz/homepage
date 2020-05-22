aa895a44c7d44453c158e31c301e037f8f5825e1
Michal Pekacz <michal.pekacz@gmail.com>
2020-05-22 23:41:59 +0200

    second commit

---
+++ main.js
@@ -0,0 +1,7 @@
+alert("Jak mnie denerwują te wszystke wyskakujące okienka!")
+const firstName = 'Michał';
+const age = 32;
+
+console.log(firstName);
+console.log(age);
+console.log(`Cześć! Nazywam się ${firstName} i mam ${32} lata`)
