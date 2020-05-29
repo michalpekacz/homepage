654c874f3462d56f617da5c5155fcbb738a48b15
Michal Pekacz <michal.pekacz@gmail.com>
2020-05-29 21:59:27 +0200

    week 4 final

--- main.js
+++ main.js
@@ -5,3 +5,6 @@ console.log(firstName);
 console.log(age);
 console.log(`Cześć! Nazywam się ${firstName} i mam ${32} lata`);
 
+const emptyHeading = document.querySelector('.main-header__heading--js');
+
+emptyHeading.innerHTML = `Ten napis został stworzony za pomocą javascriptu`
